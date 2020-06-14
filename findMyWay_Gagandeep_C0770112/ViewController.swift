a484b6684472558aa90d3549cc90b4c7b4b04dce
gagandeepkaur1510 <gagandeepkaur1510@gmail.com>
2020-06-14 17:46:44 -0400

    Added MapView

---
+++ findMyWay_Gagandeep_C0770112/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  findMyWay_Gagandeep_C0770112
+//
+//  Created by Mac on 6/14/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mMap: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
