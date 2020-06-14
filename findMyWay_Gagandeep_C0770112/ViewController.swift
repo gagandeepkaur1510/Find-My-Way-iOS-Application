415055243369e9f2cc377069d9b142d4a9410df3
gagandeepkaur1510 <gagandeepkaur1510@gmail.com>
2020-06-14 18:26:21 -0400

    Added Find My Way Location Functionality

--- findMyWay_Gagandeep_C0770112/ViewController.swift
+++ findMyWay_Gagandeep_C0770112/ViewController.swift
@@ -12,11 +12,50 @@ import MapKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mMap: MKMapView!
-    override func viewDidLoad() {
+    let mLocationManager = CLLocationManager()
+    let SPAN = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        setupLocationManager()
+        addDoubleTapGesture()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func setupLocationManager()
+    {
+        mLocationManager.desiredAccuracy = kCLLocationAccuracyBest
+        mLocationManager.requestWhenInUseAuthorization()
+    }
+    
+    func addDoubleTapGesture()
+    {
+        let tap_gesture = UITapGestureRecognizer(target: self, action: #selector(tapGestureRecognized))
+        tap_gesture.numberOfTapsRequired = 2
+        mMap.addGestureRecognizer(tap_gesture)
+    }
+    
+    @objc func tapGestureRecognized(gesture: UITapGestureRecognizer)
+    {
+        let touch_point = gesture.location(in: mMap)
+        let coordinates = mMap.convert(touch_point,toCoordinateFrom: mMap)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinates
+        mMap.addAnnotation(annotation)
+    }
+    
+    @IBAction func findMyWayClicked(_ sender: Any)
+    {
+        if let location = mLocationManager.location?.coordinate
+        {
+            let region = MKCoordinateRegion.init(center: location,span: SPAN)
+            mMap.setRegion(region, animated: true)
+        }
+    }
+    
+    @IBAction func navigateClicked(_ sender: Any) {
+    }
 }
 
